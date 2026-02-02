f165f76b37a9792aea8598c14d8e86a9425dc7dd
ry <yeryanyin@gmail.com>
2026-02-02 09:08:34 -0500

    Update app.py
    pain and suffering

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 values = [1,2.23,5,7,2,30,15]
-print(values)
+print(values[0])
+print(values[6])
 for i in values:
     print(i)
+
+number = int(input("Pick a Number"))
+if number%2 == 0:
+    print("even")
+else:
+    print("odd")
