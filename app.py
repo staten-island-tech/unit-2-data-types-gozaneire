e4b8a9a7c3cb64a6c5740489fcbdf0fb2ca657c9
ry <yeryanyin@gmail.com>
2026-02-09 09:17:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 value1 = int(input("Insert number"))
-for division in range:
-     division + 1
-     print(division)
 
-factors = value1//division
-if factors == True:
-     print[factors]
-else:
-    print("No factors")
+for division in value1:
+     value1/division
+     factors = value1/division
+     if factors == True:
+          print[factors]
+     else:
+          print("No factors")
